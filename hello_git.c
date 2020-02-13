83c08579a8fc9df80f11372860fec7a7c26197ca
Miloš Andrić <Milos.Andric@rt-rk.com>
2020-02-13 10:27:56 +0100

    Hello push again

--- hello_git.c
+++ hello_git.c
@@ -1,10 +1,11 @@
 --- a/hello_git.c
 +++ b/hello_git.c
-@@ -8,7 +8,7 @@            
+@@ -9,7 +9,7 @@            
   int main() {     
 	//Make changes to string constant parameter of printf function
--       printf("Hello git!\n");
-+	printf("Hello push!\n");
+        printf("Hello push!\n");
 
+-
++	printf("Hello push once again!\n");
 	return 0;
  }
